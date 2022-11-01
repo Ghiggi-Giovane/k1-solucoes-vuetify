f74bf214abf58984ad511154376c91d26691f74a
ghiggi-giovane <ghiggi1234@gmail.com>
2022-10-31 21:11:08 -0300

    home vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+<div id="App">
+  <MainNavbar />
+			<router-view />
+
+</div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+import MainNavbar from './components/MainNavbar';
+
+export default {
+  name: 'App',
+	components: {
+		MainNavbar,
+	}
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify/lib'
+
+
+import '@mdi/font/css/materialdesignicons.css'
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+})
